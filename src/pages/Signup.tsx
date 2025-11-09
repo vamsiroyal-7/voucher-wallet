65df76451eda61fe5479cb40fa15086b7325b17c
vamsiroyal-7 <vamsiroyal07@outlook.com>
2025-11-09 17:06:21 +0530

    Fix cloudflare build errors

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from "react";
+import type { FormEvent } from "react";
+import { useState } from "react";
 import { supabase } from "../supabase";
 import { Link, useNavigate } from "react-router-dom";
 
